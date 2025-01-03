fbedecfd8bee292ac61d7873345a0f903306bbae
Victor Ermolaev <victorermolaev@gmail.com>
2025-01-03 12:00:25 +0100

    Update paths for GH Pages.

--- src/front/openings.rs
+++ src/front/openings.rs
@@ -13,19 +13,19 @@ pub fn Openings(color: String, is_subcomponent: bool) -> Element {
             data::Opening {
                 id: 0,
                 color: PlayerColor::White,
-                img_src: "/assets/openings/white/english-opening.png".to_string(),
+                img_src: "./assets/openings/white/english-opening.png".to_string(),
                 name: "English opening".to_string(),
             },
             data::Opening {
                 id: 1,
                 color: PlayerColor::White,
-                img_src: "/assets/openings/white/italian-game.png".to_string(),
+                img_src: "./assets/openings/white/italian-game.png".to_string(),
                 name: "Italian game".to_string(),
             },
             data::Opening {
                 id: 2,
                 color: PlayerColor::White,
-                img_src: "/assets/openings/white/queens-gambit.png".to_string(),
+                img_src: "./assets/openings/white/queens-gambit.png".to_string(),
                 name: "Queen's gambit".to_string(),
             }];
 
@@ -34,7 +34,7 @@ pub fn Openings(color: String, is_subcomponent: bool) -> Element {
                 data::Opening {
                     id: 3,
                     color: PlayerColor::White,
-                    img_src: "/assets/openings/white/scotch-game.png".to_string(),
+                    img_src: "./assets/openings/white/scotch-game.png".to_string(),
                     name: "Scotch game".to_string(),
                 });
         }
@@ -45,25 +45,25 @@ pub fn Openings(color: String, is_subcomponent: bool) -> Element {
             data::Opening {
                 id: 4,
                 color: PlayerColor::Black,
-                img_src: "/assets/openings/black/caro-kann-defence.png".to_string(),
+                img_src: "./assets/openings/black/caro-kann-defence.png".to_string(),
                 name: "Caro-Kann defence".to_string(),
             },
             data::Opening {
                 id: 5,
                 color: PlayerColor::Black,
-                img_src: "/assets/openings/black/dutch-defence.png".to_string(),
+                img_src: "./assets/openings/black/dutch-defence.png".to_string(),
                 name: "Dutch defence".to_string(),
             },
             data::Opening {
                 id: 6,
                 color: PlayerColor::Black,
-                img_src: "/assets/openings/black/french-defence.png".to_string(),
+                img_src: "./assets/openings/black/french-defence.png".to_string(),
                 name: "French defence".to_string(),
             },
             data::Opening {
                 id: 7,
                 color: PlayerColor::Black,
-                img_src: "/assets/openings/black/scandinavian-defence.png".to_string(),
+                img_src: "./assets/openings/black/scandinavian-defence.png".to_string(),
                 name: "Scandinavian defence".to_string(),
             }];
 
@@ -72,7 +72,7 @@ pub fn Openings(color: String, is_subcomponent: bool) -> Element {
                 data::Opening {
                     id: 8,
                     color: PlayerColor::Black,
-                    img_src: "/assets/openings/black/sicilian-defence.png".to_string(),
+                    img_src: "./assets/openings/black/sicilian-defence.png".to_string(),
                     name: "Sicilian defence".to_string(),
                 });
         }
